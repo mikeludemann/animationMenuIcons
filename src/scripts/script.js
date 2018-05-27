07d13d2ed3fdcd19c8aebcc9bc986712f64a544c
Mike Ludemann <mike-ludemann@hotmail.de>
2018-05-27 18:01:30 +0200

    First commit with all informations

---
+++ src/scripts/script.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {
+
+    $(".container").click(function () {
+
+        $(this).toggleClass("burger_to_x"); // Replace "burger_to_x" with any other available classes for another animation
+
+    });
+
+    $(".container_2").click(function () {
+
+        $(this).toggleClass("burger_to_x"); // Replace "burger_to_x" with any other available classes for another animation
+
+    });
+
+});
